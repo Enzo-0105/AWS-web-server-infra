a1346f4b00509360866146af85bb2dce654143b6
Salako Lateef <132714825+Enzo-0105@users.noreply.github.com>
2024-10-03 20:30:53 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,15 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+        source  = "hashicorp/aws"
       version = "~>3.0"
+    }
+  }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "enzo-0105"
+    workspaces {
+      name = "jiggy"
     }
   }
 }
